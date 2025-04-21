ae4a8a53f63813f9ca879d7924d4eca244cb7cee
Sandy Cruz <sandyramoncruzmontero@gmail.com>
2025-04-21 18:16:04 -0400

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-5x1 font-bold underline" font-bold underline>
+        Sup World</h1>
+      
+    </>
+  )
+}
+
+export default App
